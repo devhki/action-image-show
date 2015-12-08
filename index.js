a3fb1a57c7c588b960d6038a7cfa1a324ddcebd1
Viljami Anttonen <viljami.anttonen@gmail.com>
2015-12-08 23:18:24 +0200

    Added more functionality, next and previous images. Toggling of description

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ class ImageShow {
 	constructor(settingsElement) {
 		let $settingsElement = $(settingsElement);
 
+		this.showing = false;
+		this.descriptionShowing = true;
 		this.unique = ImageShow.MODULE_NAME + 'c' + initCount;
 		this.linkClass = $settingsElement.data().imageShowLinkClass || ImageShow.MODULE_NAME + '__link';
-		this.overlayClass = $settingsElement.data().imageShowOverlayClass || ImageShow.MODULE_NAME + '__overlay';
+		this.overlayClass = $settingsElement.data().imageShowOverlayClass || 'overlay';
+		this.descriptionShowingClass = $settingsElement.data().imageShowDescriptionShowingClass || 'description-visible';
 		this.$overlay = null;
+		this.currentImage = null;
 		this.liveImages = [];
 		this.imageTemplateClass = $settingsElement.data().imageTemplateClass || ImageShow.MODULE_NAME + '__image-template';
+		this.loadAnimationTemplateClass = $settingsElement.data().loadAnimationTemplate || ImageShow.MODULE_NAME + '__load-animation-template';
 
 		this.images = $settingsElement.data().imageShow;
 		if (!(this.images instanceof Array) || !this.images.length) {
@@ -24,6 +29,7 @@ class ImageShow {
 			return false;
 		}
 
+		// Template for image
 		let $imageTemplate = $('script[type="text/x-template"].' + this.imageTemplateClass).first();
 		if (!$imageTemplate.length) {
 			console.warn('ImageShow: Mustache image-template needed, dying silently...');
@@ -32,9 +38,13 @@ class ImageShow {
 		this.imageTemplate = $imageTemplate.html();
 
 
-
-		console.log($settingsElement.data().imageShow);
-		console.log(this.linkClass);
+		// Template for load animation
+		let $loadAnimationTemplate = $('script[type="text/x-template"].' + this.loadAnimationTemplateClass).first();
+		if (!$loadAnimationTemplate.length) {
+			this.loadAnimationTemplate = '';
+		} else {
+			this.loadAnimationTemplate = $loadAnimationTemplate.html();
+		}
 
 		this._init();
 
@@ -45,6 +55,7 @@ class ImageShow {
 
 	show(ref) {
 		let image = this._findImage(ref);
+		this.showing = true;
 
 		if (!image.urls || !image.urls.view) {
 			console.warn('ImageShow: Image "' + image.ref + '" is missing view url (image.urls.view), not gonna do anything...');
@@ -57,11 +68,33 @@ class ImageShow {
 		}, (reason) => {
 			console.warn('ImageShow:', reason);
 		});
+
+		this.liveImages.map(($image) => {
+			$image.find('.' + ImageShow.MODULE_NAME + '__load-animation-container').html( this.loadAnimationTemplate );
+		});
+
 	}
 
 	hide() {
+		this.showing = false;
 		this._hideImages();
 		this._hideOverlay();
+	}
+
+	next() {
+		if ( !this.currentImage ) {
+			return false;
+		}
+
+		this.show( this._findNextImage().ref );
+	}
+
+	previous() {
+		if ( !this.currentImage ) {
+			return false;
+		}
+
+		this.show( this._findPreviousImage().ref );
 	}
 
 	// Private methods
@@ -82,10 +115,12 @@ class ImageShow {
 				return false;
 			}
 
+			self.descriptionShowing = true;
 			self.show(ref);
 
 			return false;
 		});
+
 	}
 
 	_findImage(ref) {
@@ -96,6 +131,42 @@ class ImageShow {
 			return false;
 		})[0];
 	}
+
+	_findNextImage() {
+		let last = null;
+		let target = null;
+
+		if ( !this.currentImage ) {
+			return false;
+		}
+
+		for ( let i = 0; i < this.images.length; i++ ) {
+			if (last && last.ref == this.currentImage.ref) {
+				return this.images[i];
+			}
+			last = this.images[i];
+		}
+
+		return this.images[0];
+	}
+
+	_findPreviousImage() {
+		let last = null;
+
+		if ( !this.currentImage ) {
+			return false;
+		}
+
+		for ( let i = 0; i < this.images.length; i++ ) {
+			if ( last && this.images[i].ref == this.currentImage.ref ) {
+				return last;
+			}
+			last = this.images[i];
+		}
+
+		return this.images[this.images.length - 1];
+	}
+
 
 	_showOverlay() {
 		if (this.$overlay) {
@@ -133,43 +204,99 @@ class ImageShow {
 	}
 
 	_showImage(image) {
+		if ( !this.showing ) {
+			return false;
+		}
+
 		let rendered = MUSTACHE.to_html( this.imageTemplate, {image:[image]} );
 		let $image = $(rendered);
+		let animate = !this.liveImages.length;
+		let clicked = false;
 
+		this._hideImages();
+
+		this.currentImage = image;
 		this.liveImages.push($image);
 
-		$image
-			.css('opacity', 0)
-			.appendTo('body');
+		$image.appendTo('body');
 
-		$image.find('.' + ImageShow.MODULE_NAME + '__close').one('click.' + this.unique, () => {
+		$image.find('.' + ImageShow.MODULE_NAME + '__close').on('click.' + this.unique, () => {
+			clicked = true;
 			this.hide();
 			return false;
 		});
 
-		ANIM.transition($image, {}, {
-			opacity: 1
-		}).then(() => {
-			$image.css('opacity', '');
+		$image.find('.' + ImageShow.MODULE_NAME + '__previous').on('click.' + this.unique, () => {
+			if ( clicked ) {
+				return false;
+			}
+			clicked = true;
+			this.previous();
+			return false;
 		});
+
+		$image.find('.' + ImageShow.MODULE_NAME + '__next').on('click.' + this.unique, () => {
+			if ( clicked ) {
+				return false;
+			}
+			clicked = true;
+			this.next();
+			return false;
+		});
+
+		$image.find('.' + ImageShow.MODULE_NAME + '__toggle-description').on('click.' + this.unique, () => {
+			this.descriptionShowing = !this.descriptionShowing;
+			this._updateDescriptionVisibility();
+			return false;
+		});
+
+		if ( animate ) {
+			ANIM.transition($image, {
+				opacity : 0
+			}, {
+				opacity: 1
+			}).then(() => {
+				$image.css('opacity', '');
+				this._updateDescriptionVisibility();
+			});
+		} else {
+			this._updateDescriptionVisibility();
+		}
 	}
 
 	_hideImages() {
 		this.liveImages.map(($image) => {
 			$image.remove();
 		});
+		while(this.liveImages.length) {
+			this.liveImages.pop();
+		}
+		this.liveImages = [];
+		this.currentImage = null;
 	}
 
 	_loadImage(url) {
 		return new Promise((resolve, reject) => {
 			let loader = new Image();
 			loader.onload = () => {
-				resolve(loader);
+				setTimeout(() => {
+					resolve(loader);
+				}, Math.random() * 1000 + 1000)
 			}
 			loader.onerror = () => {
 				reject('Couldn\'t load the image');
 			}
 			loader.src = url;
+		});
+	}
+
+	_updateDescriptionVisibility() {
+		this.liveImages.map(($image) => {
+			if (this.descriptionShowing) {
+				$image.addClass(this.descriptionShowingClass);
+			} else {
+				$image.removeClass(this.descriptionShowingClass);
+			}
 		});
 	}
 
